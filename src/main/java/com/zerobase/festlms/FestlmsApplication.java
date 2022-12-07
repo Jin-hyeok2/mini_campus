599bec5a037218baee63a7b91730a722f32560dc
DESKTOP-G85SRSM\hwlsg <hjhlove1120@gmail.com>
2022-12-08 02:33:17 +0900

    dir 변경

---
+++ src/main/java/com/zerobase/festlms/FestlmsApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.festlms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestlmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestlmsApplication.class, args);
+	}
+
+}
